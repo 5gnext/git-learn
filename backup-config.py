4261c551eabde3043640af1064dbd80606b085bc
Eric Lai <allenlaiyang@gmail.com>
2021-11-28 16:32:45 -0500

    backup in paralle

--- backup-config.py
+++ backup-config.py
@@ -1,6 +1,6 @@
 import paramiko
 import time
-
+import threading
 def connect(server_ip, server_port, user, passwd):
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -22,13 +22,8 @@ def show(shell, n=100000):
     output = shell.recv(n)
     return output.decode()
 
-def close(ssh_client):
-    if ssh_client.get_transport().is_active() == True:
-        print('closing connection')
-        ssh_client.close()
-if __name__ == '__main__':
-    router1 = {'server_ip': '10.1.1.1', 'server_port': '22', 'user':'admin', 'passwd':'admin'}
-    client = connect(**router1)
+def backup(router):
+    client = connect(**router)
     shell = get_shell(client)
     send_command(shell,'term len 0')
     send_command(shell,'enable')
@@ -39,6 +34,31 @@ if __name__ == '__main__':
     output_list = output.splitlines()
     output_list = output_list[18:-1]
     output = '\n'.join(output_list)
-    print(output)
-    f = open('r1-config-backup.txt', 'w')
+    from datetime import datetime
+    now = datetime.now()
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+    file_name = f'{router["server_ip"]}_{year}-{month}-{day}-{hour}-{minute}'
+    # print(output)
+
+    f = open(file_name, 'w')
     f.write(output)
+def close(ssh_client):
+    if ssh_client.get_transport().is_active() == True:
+        print('closing connection')
+        ssh_client.close()
+if __name__ == '__main__':
+    router1 = {'server_ip': '10.1.1.1', 'server_port': '22', 'user':'admin', 'passwd':'admin'}
+    router2 = {'server_ip': '10.1.1.3', 'server_port': '22', 'user':'admin', 'passwd':'admin'}
+    routers = [router1, router2]
+    threads = list()
+    for router in routers:
+        th = threading.Thread(target=backup, args=(router,))
+        threads.append(th)
+    for th in threads:
+        th.start()
+    for th in threads:
+        th.join()
